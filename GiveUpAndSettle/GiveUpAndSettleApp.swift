d0ec454c77ff20b528c3f2230d828d15c56b640a
Prashant Cholachagudda <pvc@outlook.com>
2021-03-18 23:16:03 +0530

    Initial Commit

---
+++ GiveUpAndSettle/GiveUpAndSettleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiveUpAndSettleApp.swift
+//  GiveUpAndSettle
+//
+//  Created by Prashant C on 18/03/21.
+//
+
+import SwiftUI
+
+@main
+struct GiveUpAndSettleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
