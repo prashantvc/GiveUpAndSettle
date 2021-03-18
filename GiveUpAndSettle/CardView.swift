ca6168055cc8a8d0a4770cbc86094215cefd32c2
Prashant Cholachagudda <pvc@outlook.com>
2021-03-19 00:02:43 +0530

    Added the swipe gesture

--- GiveUpAndSettle/CardView.swift
+++ GiveUpAndSettle/CardView.swift
@@ -8,25 +8,27 @@
 import SwiftUI
 
 struct CardView: View {
+    @State var translation: CGSize = .zero
     var body: some View {
-        GeometryReader{ geometry in
-            VStack(alignment: .leading)  {
+        GeometryReader { geometry in
+            VStack(alignment: .leading) {
                 Image(systemName: "person.fill")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: geometry.size.width, height: geometry.size.height * 0.75)
+                    .frame(width: geometry.size.width, height: geometry.size.height * 0.75) // 3
                     .clipped()
-                HStack{
+                
+                HStack {
                     VStack(alignment: .leading, spacing: 6) {
-                        Text("Maya, 1")
+                        Text("Debra Weber, 28")
                             .font(.title)
-                            .bold()
-                        Text("Tabby")
+                            .foregroundColor(.primary)
+                        Text("Judge")
                             .font(.subheadline)
-                            .bold()
-                        Text("13 Mutual Friends")
+                            .foregroundColor(.primary)
+                        Text("Last seen today")
                             .font(.subheadline)
-                            .foregroundColor(.gray)
+                            .foregroundColor(.secondary)
                     }
                     Spacer() // Add a spacer to push our HStack to the left and the spacer fill the empty space
                     
@@ -34,13 +36,26 @@ struct CardView: View {
                         .foregroundColor(.gray)
                 }.padding(.horizontal)
             }
+            // Add padding, corner radius and shadow with blur radius
             .padding(.bottom)
-            .background(Color.white)
+            .background(Color(.systemGray6))
             .cornerRadius(10)
             .shadow(radius: 5)
+            .animation(.interactiveSpring())
+            .offset(x: self.translation.width, y: 0)
+            .rotationEffect(.degrees(Double(self.translation.width / geometry.size.width) * 25), anchor: .bottom)
+            .gesture(
+                DragGesture()
+                    .onChanged{ value in
+                        self.translation = value.translation
+                    }.onEnded{ value in
+                        self.translation = .zero
+                    }
+            )
         }
     }
 }
+
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
